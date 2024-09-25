2fb3ede54049df1d0b620ca0152ae308c4ba729b
TymekGluch <78880779+TymekGluch@users.noreply.github.com>
2024-09-25 23:02:41 +0200

    fix(CarerreTimeline): Add display fixes

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -2,7 +2,7 @@ import React from "react"
 import { BREAKPOINTS_MIN_KEYS } from "../constants/breakpoints.constants"
 
 const useIsMobile = (): boolean => {
-    const [isMobile, setIsMobile] = React.useState<boolean>(false)
+    const [isMobile, setIsMobile] = React.useState<boolean>(true)
 
     React.useEffect(() => {
         const mediaQuery = window.matchMedia(BREAKPOINTS_MIN_KEYS.SMALL_DESKTOP)
